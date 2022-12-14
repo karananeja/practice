3884a615b03e995a0788f6bd591b3af1444c421a
karananeja <karananeja.r@gmail.com>
2022-12-14 20:01:48 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -213,3 +213,31 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
 	console.log(noOfApples + '\n' + noOfOranges);
 }
 countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
+
+/**
+ * @problem_eight
+ * Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. 
+ * She tabulates the number of times she breaks her season record for most points and least points in a game. 
+ * Points scored in the first game establish her record for the season, and she begins counting from there.
+ */
+/**
+ * @solution_eight
+ */
+function breakingRecords(scores) {
+	let maxTimes = 0, minTimes = 0;
+	let min = scores[0];
+	let max = scores[0];
+
+	for (let i = 0; i < scores.length; i++) {
+		if (scores[i] < min) {
+			min = scores[i];
+			minTimes++;
+		}
+		if (scores[i] > max) {
+			max = scores[i];
+			maxTimes++;
+		}
+	}
+	return [maxTimes, minTimes];
+}
+console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]));
