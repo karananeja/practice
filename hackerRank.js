2b6fe35b2c3fdd37925b1f19e0338d6c65e43a50
karananeja <karananeja.r@gmail.com>
2022-12-19 10:21:24 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -286,3 +286,31 @@ function regexNumStr(numString) {
 
 }
 console.log(!!regexNumStr('123'));
+
+/**
+ * @problem_eleven
+ * Complete the 'kangaroo' function below.
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+/**
+ * @solution_eleven
+ */
+
+function kangaroo(x1, v1, x2, v2) {
+	// Write your code here
+	if ((x1 - x2) * (v1 - v2) > 0) {
+		return "NO";
+	}
+	if ((x2 - x1) % (v1 - v2) == 0) {
+		return "YES";
+	}
+	else {
+		return "NO";
+	}
+}
+console.log(kangaroo(0, 3, 4, 2));
