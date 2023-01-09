d24609aa87111601e2bd41764b31c17680442ccb
karananeja <karananeja.r@gmail.com>
2023-01-09 13:13:04 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -511,3 +511,29 @@ function sockMerchant(n, arr) {
 	return numberOfPairs;
 }
 console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
+
+/**
+ * @problem_twenty
+ * An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly "steps" steps, for every step it was noted if it was an uphill, U, or a downhill, D step. 
+ * Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude. We define the following terms:
+ * A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+ * A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+ * Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+ */
+/**
+ * @solution_twenty
+ */
+function countingValleys(steps, path) {
+	// Write your code here
+	let uphillQuantity = 0, isUpHill = false, result = 0;
+
+	for (let i = 0; i < steps; i++) {
+		isUpHill = (path[i] === 'U');
+		isUpHill ? uphillQuantity++ : uphillQuantity--;
+
+		if (isUpHill && uphillQuantity === 0) result++;
+	}
+
+	return result;
+}
+console.log(countingValleys(8, "UDDDUDUU"));
