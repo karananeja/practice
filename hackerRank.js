83ff10cdef9ad183ef29cf339d0098338c59d63e
karananeja <karananeja.r@gmail.com>
2023-01-12 09:20:37 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -588,3 +588,19 @@ function pageCount(n, p) {
 	return result;
 }
 console.log(pageCount(6, 2));
+
+/**
+ * @problem_twentyThree
+ * A video player plays a game in which the character competes in a hurdle race. 
+ * Hurdles are of varying heights, and the characters have a maximum height they can jump. 
+ * There is a magic potion they can take that will increase their maximum jump height by 1 unit for each dose. 
+ * How many doses of the potion must the character take to be able to jump all of the hurdles. If the character can already clear all of the hurdles, return 0.
+ */
+/**
+ * @solution_twentyThree
+ */
+function hurdleRace(k, height) {
+	// Write your code here
+	return k > Math.max(...height) ? 0 : Math.abs(Math.max(...height) - k);
+}
+console.log(hurdleRace(4, [1, 6, 3, 5, 2]));
