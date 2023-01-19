7e5101478bc71e72c2c03b863346b416d71ee842
karananeja <karananeja.r@gmail.com>
2023-01-19 09:38:09 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -727,3 +727,32 @@ function angryProfessor(k, a) {
 	return arrivedEarly >= k ? 'NO' : 'YES';
 }
 console.log(angryProfessor(3, [-1, -3, 4, 2]));
+
+/**
+ * @problem_twentyNine
+ * Lily likes to play games with integers. 
+ * She has created a new game where she determines the difference between a number and its reverse. 
+ * For instance, given the number 12, its reverse is 21. Their difference is 9. The number 120 reversed is 21, and their difference is 99.
+ * She decides to apply her game to decision making. She will look at a numbered range of days and will only go to a movie on a beautiful day.
+ * Given a range of numbered days, [i...j] and a number k, determine the number of days in the range that are beautiful. 
+ * Beautiful numbers are defined as numbers where |i-reverse(i)| is evenly divisible by k. 
+ * If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range.
+ */
+/**
+ * @solution_twentyNine
+ */
+function beautifulDays(i, j, k) {
+	// Write your code here
+	let beautifulDays = 0;
+
+	for (let num = i; num <= j; num++) {
+		let reverseNum = parseInt(num.toString().split('').reverse().join(''));
+
+		let differenceOfNum = Math.abs(num - reverseNum);
+
+		if (differenceOfNum % k == 0) beautifulDays++;
+	}
+
+	return beautifulDays;
+}
+console.log(beautifulDays(20, 23, 6));
