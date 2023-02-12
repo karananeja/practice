6977c3c3b5eb7d1dd2df3899d5c8a36750a6783c
karananeja <karananeja.r@gmail.com>
2023-02-12 22:55:14 +0530

    formatted a part of the solution

--- hackerRank.js
+++ hackerRank.js
@@ -1037,7 +1037,36 @@ console.log(repeatedString('aba', 10));
  */
 function timeInWords(h, m) {
 	// Write your code here
-	const numberToWord = { 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 21: 'twenty one', 22: 'twenty two', 23: 'twenty three', 24: 'twenty four', 25: 'twenty five', 26: 'twenty six', 27: 'twenty seven', 28: 'twenty eight', 29: 'twenty nine' };
+	const numberToWord = {
+		1: 'one',
+		2: 'two',
+		3: 'three',
+		4: 'four',
+		5: 'five',
+		6: 'six',
+		7: 'seven',
+		8: 'eight',
+		9: 'nine',
+		10: 'ten',
+		11: 'eleven',
+		12: 'twelve',
+		13: 'thirteen',
+		14: 'fourteen',
+		16: 'sixteen',
+		17: 'seventeen',
+		18: 'eighteen',
+		19: 'nineteen',
+		20: 'twenty',
+		21: 'twenty one',
+		22: 'twenty two',
+		23: 'twenty three',
+		24: 'twenty four',
+		25: 'twenty five',
+		26: 'twenty six',
+		27: 'twenty seven',
+		28: 'twenty eight',
+		29: 'twenty nine',
+	};
 
 	if (m === 0) return `${numberToWord[h]} o' clock`;
 	if (m === 15) return `quarter past ${numberToWord[h]}`;
