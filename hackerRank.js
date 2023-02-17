1562568a5c6154a37b4b117f55804f341cecd8cc
karananeja <karananeja.r@gmail.com>
2023-02-17 22:26:19 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -1105,3 +1105,20 @@ console.log(serviceLane(8, [[0, 3], [4, 6], [6, 7], [3, 5], [0, 7]], [
 	2, 3, 1, 2,
 	3, 2, 3, 3
 ]));
+
+/**
+ * @problem_fortyThree
+ * There is a sequence of words in CamelCase as a string of letters, s, having the following properties:
+ * It is a concatenation of one or more words consisting of English letters.
+ * All letters in the first word are lowercase.
+ * For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+ * Given s, determine the number of words in s.
+ */
+/**
+ * @solution_fortyThree
+ */
+function camelCase(s) {
+	// Write your code here
+	return s.match(/[A-Z]/g) ? s.match(/[A-Z]/g).length + 1 : 1;
+}
+console.log(camelCase("saveChangesInTheEditor"));
