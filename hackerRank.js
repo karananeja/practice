6aa6d5a4036d90067fe59fa9fc37a1d26cbff1f5
karananeja <karananeja.r@gmail.com>
2023-02-24 21:57:46 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -1180,3 +1180,31 @@ function chocolateFeast(n, c, m) {
 	return noOfBars;
 }
 console.log(chocolateFeast(10, 2, 5));
+
+/**
+ * @problem_fortySix
+ * There is a new mobile game that starts with consecutively numbered clouds.
+ * Some of the clouds are thunderheads and others are cumulus.
+ * The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2.
+ * The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting position to the last cloud. It is always possible to win the game.
+ * For each game, you will get an array of clouds numbered 0 if they are safe or 1 if they must be avoided.
+ */
+/**
+ * @solution_fortySix
+ */
+function jumpingOnClouds(c) {
+	// Write your code here
+	let count = 0;
+
+	for (let i = 1; i < c.length; i++) {
+		if (c[i + 1] !== 1) {
+			count++;
+			i++;
+		} else {
+			count++;
+		}
+	}
+
+	return count;
+}
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
