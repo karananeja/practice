a0c435944e912f487c19a7f22e79f75a90086a06
karananeja <karananeja.r@gmail.com>
2023-04-02 22:53:18 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -1212,3 +1212,20 @@ function jumpingOnClouds(c) {
 	return count;
 }
 console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]));
+
+/**
+ * @problem_fortySeven
+ * A jail has a number of prisoners and a number of treats to pass out to them.
+ * Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs.
+ * A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+ * The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful.
+ * Determine the chair number occupied by the prisoner who will receive that candy.
+ */
+/**
+ * @solution_fortySeven
+ */
+function saveThePrisoner(n, m, s) {
+	// Write your code here
+	return (s - 1 + m) % n === 0 ? n : (s - 1 + m) % n;
+}
+console.log(saveThePrisoner(7, 19, 2));
