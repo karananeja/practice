3033db8e965ed43f30e45f08cef42181bd3de8b2
karananeja <karananeja.r@gmail.com>
2023-04-03 21:26:48 +0530

    solved a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -1229,3 +1229,31 @@ function saveThePrisoner(n, m, s) {
 	return (s - 1 + m) % n === 0 ? n : (s - 1 + m) % n;
 }
 console.log(saveThePrisoner(7, 19, 2));
+
+/**
+ * @problem_fortyEight
+ * John Watson knows of an operation called a right circular rotation on an array of integers.
+ * One rotation operation moves the last array element to the first position and shifts all remaining elements right one.
+ * To test Sherlock's abilities, Watson provides Sherlock with an array of integers.
+ * Sherlock is to perform the rotation operation a number of times then determine the value of the element at a given position.
+ * For each array, perform a number of right circular rotations and return the values of the elements at the given indices.
+ */
+/**
+ * @solution_fortyEight
+ */
+function circularArrayRotation(a, k, queries) {
+	// Write your code here
+	for (let i = 0; i < k; i++) {
+		a.unshift(a.pop());
+	}
+
+	let result = [];
+
+	for (let i of queries) {
+		result.push(a[i]);
+	}
+
+	return result;
+}
+console.log(circularArrayRotation([1, 2, 3], 2, [0, 1, 2]));
+
