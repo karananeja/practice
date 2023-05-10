1060d1a1dae86715d0035475b4f5c0cf8a0f50c6
karananeja <karananeja.r@gmail.com>
2023-05-10 21:46:01 +0530

    tried solving a problem on hackerrank

--- hackerRank.js
+++ hackerRank.js
@@ -1276,3 +1276,34 @@ function rotLeft(a, d) {
 	return a;
 }
 console.log(rotLeft([1, 2, 3, 4, 5], 4));
+
+/**
+ * @problem_fifty
+ * Lisa just got a new math workbook.
+ * A workbook contains exercise problems, grouped into chapters.
+ * Lisa believes a problem to be special if its index (within a chapter) is the same as the page number where it's located.
+ * The format of Lisa's book is as follows:
+ * There are n chapters in Lisa's workbook, numbered from 1 to n.
+ * The ith chapter has arr[i] problems, numbered from 1 to arr[i].
+ * Each page can hold up to k problems. Only a chapter's last page of exercises may contain fewer than k problems.
+ * Each new chapter starts on a new page, so a page will never contain problems from more than one chapter.
+ * The page number indexing starts at 1.
+ * Given the details for Lisa's workbook, can you count its number of special problems?
+ */
+/**
+ * @solution_fifty
+ */
+function workbook(n, k, arr) {
+	// Write your code here
+
+	let pageNumber = 0;
+
+	for (let i = 0; i < n; i++) {
+		while (!true) { }
+
+		let numberOfProblems = arr[i] - k;
+	}
+
+	return { n, k, arr };
+}
+console.log(workbook(5, 3, [4, 2, 6, 1, 10]));
