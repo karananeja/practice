8a3b62d3c61fc44bcfefdae38c8de487297bf4c8
karananeja <karananeja.r@gmail.com>
2023-07-16 21:23:15 +0530

    practising few js concepts

--- practiseJsConcepts.js
+++ practiseJsConcepts.js
@@ -8,3 +8,18 @@ buttonClick.addEventListener('click', function (e) {
   console.log('hello', e);
 }, false);
 
+
+/**
+ * Hoisting kicks in and the value of sayHello is declared as undefined so, sayHello() will throw an error as it not a function
+ */
+sayHi();
+
+function sayHi() {
+  console.log('Hi');
+}
+
+sayHello();
+
+var sayHello = function () {
+  console.log('Hello');
+};
