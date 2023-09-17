52562ace354cab5d8609a6fc455dd377ec39e350
karananeja <karananeja.r@gmail.com>
2023-09-17 20:28:48 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -913,3 +913,27 @@ function average(salary) {
   return Number(((averageSalary - minSalary - maxSalary) / (salary.length - 2)).toFixed(5));
 };
 console.log({ averageSalary: average([48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000]) });
+
+/**
+ * @param {Function} fn
+ * @returns {number}
+ */
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (key in cache) {
+      return cache[key];
+    }
+
+    const result = fn.apply(this, args);
+    cache[key] = result;
+
+    return result;
+  };
+}
+const memoizedSum = memoize((a, b) => a + b);
+console.log(memoizedSum(2, 3)); // Output: Computing sum, 5
+console.log(memoizedSum(2, 3)); // Output: 5
