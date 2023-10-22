306120054fc85e6d97185c8347ec65051dcec822
karananeja <karananeja.r@gmail.com>
2023-10-22 22:58:31 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -1229,3 +1229,17 @@ function spiralOrder(matrix) {
   return spiralMatrix;
 }
 console.log({ spiralPattern: spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) });
+
+class NumArray {
+  constructor(nums) {
+    this.prefixSum = [0];
+
+    for (let i = 0; i < nums.length; i++) {
+      this.prefixSum[i + 1] = this.prefixSum[i] + nums[i];
+    }
+  }
+
+  sumRange(left, right) {
+    return this.prefixSum[right + 1] - this.prefixSum[left];
+  }
+}
