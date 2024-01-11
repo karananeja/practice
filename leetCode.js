66b76d84a6441682032de9f975788a52691c27d7
karananeja <karananeja.r@gmail.com>
2024-01-11 22:49:27 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -1973,3 +1973,38 @@ class MyLinkedList {
     }
   }
 }
+
+class ListNode {
+  constructor(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+  }
+}
+
+/**
+ * @param {ListNode | null} list1 
+ * @param {ListNode | null} list2 
+ * @returns {ListNode | null}
+ */
+function mergeTwoLists(list1, list2) {
+  if (!list1 || !list2) return list1 || list2;
+
+  const mergedList = new ListNode();
+  let current = mergedList;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  current.next = list1 ?? list2;
+
+  return mergedList.next;
+};
