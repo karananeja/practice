fd44997812472b046b74271724b861958a4a315a
karananeja <karananeja.r@gmail.com>
2024-01-15 18:52:36 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -2023,3 +2023,34 @@ function reverseList(head) {
 
   return previous;
 };
+
+/**
+ * @param {ListNode | null} head 
+ * @param {number} left 
+ * @param {number} right 
+ * @returns {ListNode | null}
+ */
+function reverseBetween(head, left, right) {
+  let currNode = head, start = head, counter = 1;
+
+  while (counter < left) {
+    start = currNode;
+    currNode = currNode.next;
+    counter++;
+  }
+
+  let tail = currNode, prev = null;
+
+  while (counter >= left && counter <= right) {
+    const next = currNode.next;
+    currNode.next = prev;
+    prev = currNode;
+    currNode = next;
+    counter++;
+  }
+
+  start.next = prev;
+  tail.next = currNode;
+
+  return left > 1 ? head : prev;
+};
