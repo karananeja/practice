7469f32cbf52eae68b5d26977546138b6a3d5492
karananeja <karananeja.r@gmail.com>
2024-01-18 23:15:25 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -2094,3 +2094,21 @@ function middleNode(head) {
 
   return head;
 };
+
+/**
+ * @param {ListNode | null} head 
+ * @returns {ListNode | null}
+ */
+function deleteDuplicates(head) {
+  let node = head;
+
+  while (node) {
+    if (node.next && node.val === node.next.val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+
+  return head;
+};
