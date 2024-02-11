10daa13fcda59837245a719531d8020184170b6f
karananeja <karananeja.r@gmail.com>
2024-02-11 19:42:13 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -2243,3 +2243,31 @@ function findMid(head) {
 
   return slow;
 };
+
+/**
+ * @param {ListNode | null} list1 
+ * @param {ListNode | null} list2 
+ * @returns {ListNode | null}
+ */
+function merge(list1, list2) {
+  if (!list1 || !list2) return list1 || list2;
+
+  const mergedList = new ListNode();
+  let current = mergedList;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+
+    current = current.next;
+  }
+
+  current.next = list1 ?? list2;
+
+  return mergedList.next;
+};
