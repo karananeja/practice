4f2e1ba123b4d412407fdddfbef4d78d53461990
karananeja <karananeja.r@gmail.com>
2024-05-16 08:49:27 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -3782,3 +3782,43 @@ function maxProductDifference(nums) {
   return (firstLargestNum * secondLargestNum) - (firstSmallestNum * secondSmallestNum);
 };
 console.log({ maxProductDifference: maxProductDifference([5, 6, 2, 7, 4]) });
+
+/**
+ * @param {number[][]} img 
+ * @returns {number[][]}
+ */
+function imageSmoother(img) {
+  // Save the dimensions of the image.
+  const m = img.length;
+  const n = img[0].length;
+
+  // Create a new image of the same dimension as the input image.
+  const smoothImg = new Array(m).fill(0).map(() => new Array(n).fill(0));
+
+  // Iterate over the cells of the image.
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      // Initialize the sum and count 
+      let sum = 0;
+      let count = 0;
+
+      // Iterate over all plausible nine indices.
+      for (let x = i - 1; x <= i + 1; x++) {
+        for (let y = j - 1; y <= j + 1; y++) {
+          // If the indices form valid neighbor
+          if (0 <= x && x < m && 0 <= y && y < n) {
+            sum += img[x][y];
+            count += 1;
+          }
+        }
+      }
+
+      // Store the rounded down value in smoothImg[i][j].
+      smoothImg[i][j] = Math.floor(sum / count);
+    }
+  }
+
+  // Return the smooth image.
+  return smoothImg;
+};
+console.log({ imageSmoother: imageSmoother([[1, 1, 1], [1, 0, 1], [1, 1, 1]]) });
