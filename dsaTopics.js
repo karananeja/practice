dff9a8426ff603a5f0a4c8a6128036330b5f08c1
karananeja <karananeja.r@gmail.com>
2024-06-06 09:59:46 +0530

    implemented heap class and max heap function

---
+++ dsaTopics.js
@@ -0,0 +1,91 @@
+class Heap {
+  constructor(heapSize) {
+    this.values = new Array(heapSize);
+    this.values[0] = -1;
+    this.size = 0;
+  }
+
+  insert(value) {
+    let currentIndex = ++this.size;
+    this.values[currentIndex] = value;
+
+    while (currentIndex > 1) {
+      const parentIndex = Math.floor(currentIndex / 2);
+
+      if (this.values[parentIndex] < this.values[currentIndex]) {
+        const temp = this.values[parentIndex];
+        this.values[parentIndex] = this.values[currentIndex];
+        this.values[currentIndex] = temp;
+        currentIndex = parentIndex;
+      } else return;
+    }
+  }
+
+  log() {
+    let idx = 1;
+
+    while (idx <= this.size) {
+      console.log(this.values[idx++]);
+    }
+  }
+
+  delete() {
+    if (this.size === 0) {
+      console.log('nothing to delete');
+      return;
+    }
+
+    this.values[1] = this.values[this.size];
+    delete this.values[this.size--];
+
+    let idx = 1;
+
+    while (idx < this.size) {
+      const leftIndex = 2 * idx,
+        rightIndex = 2 * idx + 1;
+
+      if (leftIndex < this.size && this.values[idx] < this.values[leftIndex]) {
+        [this.values[leftIndex], this.values[idx]] = [
+          this.values[idx],
+          this.values[leftIndex],
+        ];
+        idx = leftIndex;
+      } else if (
+        rightIndex < this.size &&
+        this.values[idx] < this.values[rightIndex]
+      ) {
+        [this.values[rightIndex], this.values[idx]] = [
+          this.values[idx],
+          this.values[rightIndex],
+        ];
+        idx = rightIndex;
+      } else return;
+    }
+  }
+}
+const testHeap = new Heap(10);
+
+function heapify(nums, idx, heapSize) {
+  let largest = idx;
+  const left = 2 * idx + 1,
+    right = 2 * idx + 2;
+
+  if (left < heapSize && nums[largest] < nums[left]) largest = left;
+  if (right < heapSize && nums[largest] < nums[right]) largest = right;
+
+  if (largest !== idx) {
+    [nums[largest], nums[idx]] = [nums[idx], nums[largest]];
+    heapify(nums, largest, heapSize);
+  }
+}
+
+function buildMaxHeap(nums) {
+  for (let i = Math.floor(nums.length / 2); i >= 0; i--) {
+    heapify(nums, i, nums.length);
+  }
+}
+
+const nums = [54, 53, 55, 52, 50];
+
+buildMaxHeap(nums);
+console.log({ nums });
