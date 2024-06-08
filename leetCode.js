7c0b15708d2adb82d957209de54f3fbc85e3a6c8
karananeja <karananeja.r@gmail.com>
2024-06-08 09:52:32 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -4488,3 +4488,38 @@ class RandomizedSet {
     return this.valList[Math.floor(Math.random() * this.valList.length)];
   }
 }
+
+/**
+ * @param {TreeNode | null} root 
+ * @returns {number[][]}
+ */
+function zigzagLevelOrder(root) {
+  const result = [];
+  if (!root) return result;
+
+  const currentQueue = [root];
+
+  let leftToRight = true;
+
+  while (currentQueue.length) {
+    const size = currentQueue.length;
+    const currentRow = [];
+
+    for (let idx = 0; idx < size; idx++) {
+      const frontNode = currentQueue.shift();
+      const index = leftToRight ? idx : size - idx - 1;
+      currentRow[index] = frontNode.val;
+
+      if (frontNode.left)
+        currentQueue.push(frontNode.left);
+
+      if (frontNode.right)
+        currentQueue.push(frontNode.right);
+    }
+
+    leftToRight = !leftToRight;
+    result.push(currentRow);
+  }
+
+  return result;
+};
