2d2ea0da6bc879a7916d022710a31c5ca927792c
karananeja <karananeja.r@gmail.com>
2024-06-13 07:44:30 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -4613,3 +4613,22 @@ function dailyTemperatures(temperatures) {
   return minDays;
 };
 console.log({ dailyTemperatures: dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]) });
+
+/**
+ * @param {number[]} nums 
+ * @param {number} k 
+ * @returns {number[][]}
+ */
+function divideArray(nums, k) {
+  nums.sort((a, b) => a - b);
+
+  const ans = [];
+
+  for (let i = 0; i < nums.length; i += 3) {
+    if (nums[i + 2] - nums[i] > k) return [];
+    ans.push([nums[i], nums[i + 1], nums[i + 2]]);
+  }
+
+  return ans;
+};
+console.log({ divideArray: divideArray([1, 3, 4, 8, 7, 9, 3, 5, 1], 2) });
