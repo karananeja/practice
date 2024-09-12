1ab68f4335f702c22c61eb6c6833c4a94d11380d
karananeja <karananeja.r@gmail.com>
2024-09-12 09:30:44 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -6991,3 +6991,33 @@ function minMaxGame(nums) {
   return nums[0];
 }
 console.log({ minMaxGame: minMaxGame([1, 3, 5, 2, 4, 8, 2, 2]) });
+
+/**
+ * @param {number[]} nums1 
+ * @param {number[]} nums2 
+ * @param {number[]} nums3 
+ * @returns {number[]}
+ */
+function twoOutOfThree(nums1, nums2, nums3) {
+  const numCount = {}, result = [];
+  const set1 = new Set(nums1), set2 = new Set(nums2), set3 = new Set(nums3);
+
+  for (const num of set1) {
+    numCount[num] = (numCount[num] || 0) + 1;
+  }
+
+  for (const num of set2) {
+    numCount[num] = (numCount[num] || 0) + 1;
+  }
+
+  for (const num of set3) {
+    numCount[num] = (numCount[num] || 0) + 1;
+  }
+
+  for (const num in numCount) {
+    if (numCount[num] >= 2) result.push(+num);
+  }
+
+  return result;
+};
+console.log({ twoOutOfThree: twoOutOfThree([1, 1, 3, 2], [2, 3], [3]) });
