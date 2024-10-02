eaa2f23dc67a70c54da54b7562cdbd26af6a8b8c
karananeja <karananeja.r@gmail.com>
2024-10-02 18:13:45 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -7391,3 +7391,30 @@ function reverseVowels(s) {
   return chars.join("");
 }
 console.log({ reverseVowels: reverseVowels("IceCreAm") });
+
+/**
+ * @param {string} s 
+ */
+function reverseString(s) {
+  let left = 0, right = s.length - 1;
+
+  while (left <= right) {
+    [s[left++], s[right--]] = [s[right], s[left]];
+  }
+}
+
+/**
+ * @param {string} s 
+ * @returns {string}
+ */
+function finalString(s) {
+  const result = [];
+
+  for (const char of s) {
+    if (char === "i") reverseString(result);
+    else result.push(char);
+  }
+
+  return result.join("");
+};
+console.log({ finalString: finalString("leetcode is the best platform") });
