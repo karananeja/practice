9a852f17ef1b7f80ca0c083ca160a5960cd30ad6
karananeja <karananeja.r@gmail.com>
2024-11-17 13:30:30 +0530

    implemented max heap class

--- dsaTopics.js
+++ dsaTopics.js
@@ -167,3 +167,71 @@ export class MinHeap {
     }
   }
 }
+
+export class MaxHeap {
+  constructor(compare) {
+    this.compare = compare;
+    this.heap = [];
+  }
+
+  push(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+  }
+
+  pop() {
+    if (this.heap.length === 0) return undefined;
+    const root = this.heap[0];
+    const last = this.heap.pop();
+
+    if (this.heap.length > 0 && last !== undefined) {
+      this.heap[0] = last;
+      this.heapifyDown();
+    }
+
+    return root;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  get size() {
+    return this.heap.length;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.compare(this.heap[index], this.heap[parentIndex]) <= 0) break;
+      [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let largest = index;
+
+      if (leftChildIndex < length && this.compare(this.heap[leftChildIndex], this.heap[largest]) > 0) {
+        largest = leftChildIndex;
+      }
+
+      if (rightChildIndex < length && this.compare(this.heap[rightChildIndex], this.heap[largest]) > 0) {
+        largest = rightChildIndex;
+      }
+
+      if (largest === index) break;
+
+      [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
+      index = largest;
+    }
+  }
+}
