c247f5cc5a5fb02e2e60f1c14e0972d3d01643a6
karananeja <karananeja.r@gmail.com>
2025-03-04 09:06:06 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -11104,3 +11104,19 @@ function kthLargestLevelSum(root, k) {
 
   return maxHeap.peek();
 }
+
+/**
+ * @param {TreeNode | null} root1 
+ * @param {TreeNode | null} root2 
+ * @returns {boolean}
+ */
+function flipEquiv(root1, root2) {
+  if (!root1 && !root2) return true;
+  if (!root1 || !root2) return false;
+  if (root1.val !== root2.val) return false;
+
+  return (
+    (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+    (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+  );
+}
