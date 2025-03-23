8dec21cb78db3c72f6afedcb2185692caeffd3a7
karananeja <karananeja.r@gmail.com>
2025-03-23 12:22:49 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -11530,3 +11530,44 @@ function isSymmetric(root) {
 
   return isLevelSymmetric(nodeValues);
 }
+
+/**
+ * @param {number} x 
+ * @returns {boolean}
+ */
+function checkPrime(x) {
+  for (let i = 2; i <= Math.sqrt(x); i++) {
+    if (x % i === 0) return false;
+  }
+
+  return true;
+}
+
+/**
+ * @param {number[]} nums 
+ * @returns {boolean}
+ */
+function primeSubOperation(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let bound;
+
+    if (i === 0) bound = nums[0];
+    else bound = nums[i] - nums[i - 1];
+
+    if (bound <= 0) return false;
+
+    let largestPrime = 0;
+
+    for (let j = bound - 1; j >= 2; j--) {
+      if (checkPrime(j)) {
+        largestPrime = j;
+        break;
+      }
+    }
+
+    nums[i] = nums[i] - largestPrime;
+  }
+
+  return true;
+}
+console.log({ primeSubOperation: primeSubOperation([6, 8, 11, 12]) });
