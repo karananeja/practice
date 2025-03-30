73ce147d4da6abb928bf7bfc09a1e3c31111ab77
karananeja <karananeja.r@gmail.com>
2025-03-30 08:40:06 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -11740,3 +11740,29 @@ function minimizedMaximum(n, quantities) {
   return result;
 }
 console.log({ minimizedMaximum: minimizedMaximum(6, [11, 6]) });
+
+/**
+ * @param {number[]} arr 
+ * @returns {number}
+ */
+function findLengthOfShortestSubarray(arr) {
+  let right = arr.length - 1;
+
+  while (right > 0 && arr[right] >= arr[right - 1]) {
+    right--;
+  }
+
+  let ans = right, left = 0;
+
+  while (left < right && (left === 0 || arr[left - 1] <= arr[left])) {
+    while (right < arr.length && arr[left] > arr[right]) {
+      right++;
+    }
+
+    ans = Math.min(ans, right - left - 1);
+    left++;
+  }
+
+  return ans;
+}
+console.log({ findLengthOfShortestSubarray: findLengthOfShortestSubarray([1, 2, 3, 10, 4, 2, 3, 5]) });
