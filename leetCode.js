8dded86469761c95be6914bf5bf1e3ef55e6b15a
karananeja <karananeja.r@gmail.com>
2025-05-15 10:22:46 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -12894,3 +12894,23 @@ function minOperations(nums, k) {
   }, 0);
 }
 console.log({ minOperations: minOperations([2, 11, 10, 1, 3], 10) });
+
+/**
+ * @param {number[]} nums 
+ * @param {number} target 
+ * @returns {number}
+ */
+function searchInsert(nums, target) {
+  let start = 0, end = nums.length - 1;
+
+  while (start <= end) {
+    const mid = start + Math.floor((end - start) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) start = mid + 1;
+    else end = mid - 1;
+  }
+
+  return start;
+}
+console.log({ searchInsert: searchInsert([1, 3, 5, 6], 5) });
