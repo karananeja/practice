e6ed88e7500e5b970aa396d7725fd77904ee4db0
karananeja <karananeja.r@gmail.com>
2025-07-22 11:55:34 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -14422,3 +14422,28 @@ function minOperations(nums) {
   return nums.every(num => num === 1) ? count : -1;
 }
 console.log({ minOperations: minOperations([0, 1, 1, 1, 0, 0]) });
+
+/**
+ * @param {number[]} arr 
+ * @param {number} a 
+ * @param {number} b 
+ * @param {number} c 
+ * @returns {number} 
+ */
+function countGoodTriplets(arr, a, b, c) {
+  let count = 0;
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (Math.abs(arr[i] - arr[j]) > a) continue;
+
+      for (let k = j + 1; k < n; k++) {
+        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) count++;
+      }
+    }
+  }
+
+  return count;
+}
+console.log({ countGoodTriplets: countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3) });
