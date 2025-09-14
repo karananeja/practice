4eae4fc002fa60614bc687b5e285d2d4200e146f
karananeja <karananeja.r@gmail.com>
2025-09-15 02:27:31 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -15568,3 +15568,32 @@ function triangleType(nums) {
   else return "scalene";
 }
 console.log({ triangleType: triangleType([3, 3, 3]) });
+
+/**
+ * @param {number[]} nums 
+ * @param {number[][]} queries 
+ * @returns {boolean}
+ */
+function isZeroArray(nums, queries) {
+  const deltaArray = new Array(nums.length + 1).fill(0);
+
+  for (const [left, right] of queries) {
+    deltaArray[left] += 1;
+    deltaArray[right + 1] -= 1;
+  }
+
+  const operationCounts = [];
+  let currentOperations = 0;
+
+  for (const delta of deltaArray) {
+    currentOperations += delta;
+    operationCounts.push(currentOperations);
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (operationCounts[i] < nums[i]) return false;
+  }
+
+  return true;
+}
+console.log({ isZeroArray: isZeroArray([1, 0, 1], [[0, 2]]) });
