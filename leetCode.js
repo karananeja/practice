f0211280cf0c638ed33ec88495653aa625f1f8f0
karananeja <karananeja.r@gmail.com>
2025-10-02 10:46:40 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -15969,3 +15969,26 @@ function maxDifference(s) {
   return oddCount - evenCount;
 }
 console.log({ maxDifference: maxDifference("aaaaabbc") });
+
+/**
+ * @param {number[]} flowerbed 
+ * @param {number} n 
+ * @returns {boolean}
+ */
+function canPlaceFlowers(flowerbed, n) {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0) {
+      const emptyLeft = (i === 0) || (flowerbed[i - 1] === 0);
+      const emptyRight = (i === flowerbed.length - 1) || (flowerbed[i + 1] === 0);
+
+      if (emptyLeft && emptyRight) {
+        flowerbed[i] = 1;
+        n--;
+        if (n === 0) return true;
+      }
+    }
+  }
+
+  return n <= 0;
+}
+console.log({ canPlaceFlowers: canPlaceFlowers([1, 0, 0, 0, 1], 1) });
