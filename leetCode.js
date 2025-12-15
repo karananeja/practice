ad5c63da181d6f6d0832f70a3c861a349dec0ad8
karananeja <karananeja.r@gmail.com>
2025-12-15 12:15:58 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -17663,3 +17663,34 @@ function sumOfEncryptedInt(nums) {
   return sum;
 }
 console.log({ sumOfEncryptedInt: sumOfEncryptedInt([10, 21, 31]) });
+
+class Bank {
+  constructor(balance) {
+    this.balance = balance;
+  }
+
+  transfer(account1, account2, money) {
+    if (
+      account1 > this.balance.length ||
+      account2 > this.balance.length ||
+      this.balance[account1 - 1] < money
+    ) {
+      return false;
+    }
+    this.balance[account2 - 1] += money;
+    this.balance[account1 - 1] -= money;
+    return true;
+  }
+
+  deposit(account, money) {
+    if (account > this.balance.length) return false;
+    this.balance[account - 1] += money;
+    return true;
+  }
+
+  withdraw(account, money) {
+    if (account > this.balance.length || this.balance[account - 1] < money) return false;
+    this.balance[account - 1] -= money;
+    return true;
+  }
+}
