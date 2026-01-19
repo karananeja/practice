65f48422841344f183b4dabf69194e9c511cbec1
karananeja <karananeja.r@gmail.com>
2026-01-19 18:07:35 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -18431,3 +18431,29 @@ function maxCoins(piles) {
   return count;
 }
 console.log({ maxCoins: maxCoins([2, 4, 1, 2, 7, 8]) });
+
+/**
+ * @param {string} directions 
+ * @returns {number}
+ */
+function countCollisions(directions) {
+  const n = directions.length;
+  let l = 0, r = n - 1;
+
+  while (l < n && directions[l] === 'L') {
+    l++;
+  }
+
+  while (r >= l && directions[r] === 'R') {
+    r--;
+  }
+
+  let res = 0;
+
+  for (let i = l; i <= r; i++) {
+    if (directions[i] !== 'S') res++;
+  }
+
+  return res;
+}
+console.log({ countCollisions: countCollisions("RLRSLL") });
