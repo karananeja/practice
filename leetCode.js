27d9fd17d5c8dd438eb4e208ab2e8ff40c1096ff
karananeja <karananeja.r@gmail.com>
2026-01-24 11:19:48 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -18560,3 +18560,52 @@ function countCoveredBuildings(n, buildings) {
   return count;
 }
 console.log({ countCoveredBuildings: countCoveredBuildings(3, [[1, 1], [1, 2], [2, 1], [2, 2]]) });
+
+/**
+ * @param {number} numberOfUsers 
+ * @param {string[][]} events 
+ * @returns {number[]}
+ */
+function countMentions(numberOfUsers, events) {
+  events.sort((a, b) => {
+    const timeA = parseInt(a[1]);
+    const timeB = parseInt(b[1]);
+    if (timeA !== timeB) return timeA - timeB;
+    return (b[0] === "MESSAGE" ? 0 : 1) - (a[0] === "MESSAGE" ? 0 : 1);
+  });
+
+  const count = new Array(numberOfUsers).fill(0);
+  const nextOnlineTime = new Array(numberOfUsers).fill(0);
+
+  for (const event of events) {
+    const curTime = parseInt(event[1]);
+    const type = event[0];
+
+    if (type === "MESSAGE") {
+      const target = event[2];
+      if (target === "ALL") {
+        for (let i = 0; i < numberOfUsers; i++) {
+          count[i]++;
+        }
+      } else if (target === "HERE") {
+        for (let i = 0; i < numberOfUsers; i++) {
+          if (nextOnlineTime[i] <= curTime) {
+            count[i]++;
+          }
+        }
+      } else {
+        const users = target.split(" ");
+        for (const user of users) {
+          const idx = parseInt(user.substring(2));
+          count[idx]++;
+        }
+      }
+    } else {
+      const idx = parseInt(event[2]);
+      nextOnlineTime[idx] = curTime + 60;
+    }
+  }
+
+  return count;
+}
+console.log({ countMentions: countMentions(2, [["MESSAGE", "10", "id1 id0"], ["OFFLINE", "11", "0"], ["MESSAGE", "71", "HERE"]]) });
