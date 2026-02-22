eb90199a3625dbda1cc41a9fc27ede139e3be1a6
karananeja <karananeja.r@gmail.com>
2026-02-22 11:40:54 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -19366,3 +19366,74 @@ function largestSquareArea(bottomLeft, topRight) {
   return maxSide * maxSide;
 }
 console.log({ largestSquareArea: largestSquareArea([[1, 1], [2, 2], [3, 1]], [[3, 3], [4, 4], [6, 6]]) });
+
+/**
+ * @param {number[][]} grid 
+ * @returns {number}
+ */
+function largestMagicSquare(grid) {
+  const m = grid.length, n = grid[0].length;
+
+  const rowSum = Array.from({ length: m }, () => new Array(n));
+  for (let i = 0; i < m; i++) {
+    rowSum[i][0] = grid[i][0];
+    for (let j = 1; j < n; j++) {
+      rowSum[i][j] = rowSum[i][j - 1] + grid[i][j];
+    }
+  }
+
+  const colSum = Array.from({ length: m }, () => new Array(n));
+  for (let j = 0; j < n; j++) {
+    colSum[0][j] = grid[0][j];
+    for (let i = 1; i < m; i++) {
+      colSum[i][j] = colSum[i - 1][j] + grid[i][j];
+    }
+  }
+
+  for (let edge = Math.min(m, n); edge >= 2; --edge) {
+    for (let i = 0; i + edge <= m; ++i) {
+      for (let j = 0; j + edge <= n; ++j) {
+        const stdSum = rowSum[i][j + edge - 1] - (j > 0 ? rowSum[i][j - 1] : 0);
+        let check = true;
+
+        for (let ii = i + 1; ii < i + edge; ++ii) {
+          const sum =
+            rowSum[ii][j + edge - 1] -
+            (j > 0 ? rowSum[ii][j - 1] : 0);
+
+          if (sum !== stdSum) {
+            check = false;
+            break;
+          }
+        }
+
+        if (!check) continue;
+
+        for (let jj = j; jj < j + edge; ++jj) {
+          const sum =
+            colSum[i + edge - 1][jj] -
+            (i > 0 ? colSum[i - 1][jj] : 0);
+
+          if (sum !== stdSum) {
+            check = false;
+            break;
+          }
+        }
+
+        if (!check) continue;
+
+        let d1 = 0, d2 = 0;
+
+        for (let k = 0; k < edge; ++k) {
+          d1 += grid[i + k][j + k];
+          d2 += grid[i + k][j + edge - 1 - k];
+        }
+
+        if (d1 === stdSum && d2 === stdSum) return edge;
+      }
+    }
+  }
+
+  return 1;
+}
+console.log({ largestMagicSquare: largestMagicSquare([[5, 1, 3, 1], [9, 3, 3, 1], [1, 3, 3, 8]]) });
