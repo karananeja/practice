24b45b880b90123bfb67d00fb42acce2bf730dda
karananeja <karananeja.r@gmail.com>
2026-02-26 10:08:18 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -19512,3 +19512,37 @@ function minBitwiseArray(nums) {
   return nums;
 }
 console.log({ minBitwiseArray: minBitwiseArray([2, 3, 5, 7]) });
+
+/**
+ * @param {number[]} nums 
+ * @returns {number}
+ */
+function minimumPairRemoval(nums) {
+  let count = 0;
+
+  while (nums.length > 1) {
+    let isAscending = true;
+    let minSum = Infinity;
+    let targetIndex = -1;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+      const sum = nums[i] + nums[i + 1];
+
+      if (nums[i] > nums[i + 1]) isAscending = false;
+
+      if (sum < minSum) {
+        minSum = sum;
+        targetIndex = i;
+      }
+    }
+
+    if (isAscending) break;
+
+    count++;
+    nums[targetIndex] = minSum;
+    nums.splice(targetIndex + 1, 1);
+  }
+
+  return count;
+}
+console.log({ minimumPairRemoval: minimumPairRemoval([5, 2, 3, 1]) });
