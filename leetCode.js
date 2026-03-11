06c5c57c72d504d284ee825fe1d7e16f274cf2c3
karananeja <karananeja.r@gmail.com>
2026-03-11 09:39:53 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -19800,3 +19800,23 @@ function scoreBalance(s) {
   return false;
 }
 console.log({ scoreBalance: scoreBalance("adcb") });
+
+/**
+ * @param {string[]} letters 
+ * @param {string} target 
+ * @returns {string}
+ */
+function nextGreatestLetter(letters, target) {
+  let left = 0;
+  let right = letters.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (letters[mid] <= target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return letters[left % letters.length];
+}
+console.log({ nextGreatestLetter: nextGreatestLetter(["c", "f", "j"], "a") });
