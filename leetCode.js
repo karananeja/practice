1f529990e2054089fb039c3d105dd8f3cf7a4b74
karananeja <karananeja.r@gmail.com>
2026-03-28 10:56:12 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -20167,3 +20167,24 @@ function hasAlternatingBits(n) {
   return true;
 }
 console.log({ hasAlternatingBits: hasAlternatingBits(5) });
+
+/**
+ * @param {string} s 
+ * @returns {number}
+ */
+function countBinarySubstrings(s) {
+  let ans = 0, prev = 0, cur = 1;
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i - 1] !== s[i]) {
+      ans += Math.min(prev, cur);
+      prev = cur;
+      cur = 1;
+    } else {
+      cur++;
+    }
+  }
+
+  return ans + Math.min(prev, cur);
+}
+console.log({ countBinarySubstrings: countBinarySubstrings("00110011") });
