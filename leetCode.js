a300b104ab40db6184dfe17d3c4a4f45bdcece36
karananeja <karananeja.r@gmail.com>
2026-04-25 12:30:56 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -20607,3 +20607,29 @@ function projectionArea(grid) {
   return totalArea;
 }
 console.log({ projectionArea: projectionArea([[2]]) });
+
+/**
+ * @param {number[][]} grid 
+ * @param {number} k 
+ * @returns {number}
+ */
+function countSubmatrices(grid, k) {
+  const n = grid.length;
+  const m = grid[0].length;
+  const cols = new Array(m).fill(0);
+  let res = 0;
+
+  for (let i = 0; i < n; i++) {
+    let rows = 0;
+
+    for (let j = 0; j < m; j++) {
+      cols[j] += grid[i][j];
+      rows += cols[j];
+
+      if (rows <= k) res++;
+    }
+  }
+
+  return res;
+}
+console.log({ countSubmatrices: countSubmatrices([[7, 6, 3], [6, 6, 1]], 18) });
