1bb9b5be5d378377aed8e90d96eccb3fa6fba6c2
karananeja <karananeja.r@gmail.com>
2026-05-22 10:21:11 +0530

    solved a problem on geeksforgeeks

--- geeksForGeeks.js
+++ geeksForGeeks.js
@@ -1310,3 +1310,34 @@ class RatInMaze {
     return paths;
   }
 }
+
+/**
+ * @problem_thirtySix Given a connected undirected graph containing V vertices, represented by a 2-d adjacency list adj[][], where each adj[i] represents the list of vertices connected to vertex i. Perform a Breadth First Search (BFS) traversal starting from vertex 0, visiting vertices from left to right according to the given adjacency list, and return a list containing the BFS traversal of the graph.
+
+Note: Do traverse in the same order as they are in the given adjacency list.
+ */
+/**
+ * @solution_thirtySix
+ */
+class BFS {
+  bfs(adj) {
+    // code here
+    const queue = [0];
+    const result = [];
+    const visited = new Set([0]);
+
+    while (queue.length) {
+      const frontNode = queue.shift();
+      result.push(frontNode);
+
+      for (const neighbour of adj[frontNode]) {
+        if (!visited.has(neighbour)) {
+          visited.add(neighbour);
+          queue.push(neighbour);
+        }
+      }
+    }
+
+    return result;
+  }
+}
