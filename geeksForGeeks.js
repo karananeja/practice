635cb8926bb59d1ee75e79d0f41c641824452168
karananeja <karananeja.r@gmail.com>
2026-05-23 15:02:55 +0530

    solved a problem on geeksforgeeks

--- geeksForGeeks.js
+++ geeksForGeeks.js
@@ -1341,3 +1341,34 @@ class BFS {
     return result;
   }
 }
+
+/**
+ * @problem_thirtySeven Given a connected undirected graph containing V vertices represented by a 2-d adjacency list adj[][], where each adj[i] represents the list of vertices connected to vertex i. Perform a Depth First Search (DFS) traversal starting from vertex 0, visiting vertices from left to right as per the given adjacency list, and return a list containing the DFS traversal of the graph.
+
+Note: Do traverse in the same order as they are in the given adjacency list.
+ */
+/**
+ * @solution_thirtySeven
+ */
+class DFS {
+  dfs(adj) {
+    // code here
+    const visited = new Set();
+    const result = [];
+
+    function traverse(node) {
+      visited.add(node);
+      result.push(node);
+
+      for (const neighbour of adj[node]) {
+        if (!visited.has(neighbour)) {
+          traverse(neighbour);
+        }
+      }
+    }
+
+    traverse(0);
+
+    return result;
+  }
+}
