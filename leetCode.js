01af2517207b9c02a011fd33a138c1c5d988408c
karananeja <karananeja.r@gmail.com>
2026-08-04 09:35:30 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -23090,3 +23090,40 @@ function minimumCost(cost) {
   return totalCost;
 }
 console.log({ minimumCost: minimumCost([5, 5]) });
+
+/**
+ * @param {number[]} start1
+ * @param {number[]} duration1
+ * @param {number[]} start2
+ * @param {number[]} duration2
+ * @returns {number}
+ */
+function solve(start1, duration1, start2, duration2) {
+  let finish1 = Infinity;
+
+  for (let i = 0; i < start1.length; i++) {
+    finish1 = Math.min(finish1, start1[i] + duration1[i]);
+  }
+
+  let finish2 = Infinity;
+
+  for (let i = 0; i < start2.length; i++) {
+    finish2 = Math.min(finish2, Math.max(start2[i], finish1) + duration2[i]);
+  }
+
+  return finish2;
+}
+
+/**
+ * @param {number[]} landStartTime
+ * @param {number[]} landDuration
+ * @param {number[]} waterStartTime
+ * @param {number[]} waterDuration
+ * @returns {number}
+ */
+function earliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration) {
+  const landWater = solve(landStartTime, landDuration, waterStartTime, waterDuration);
+  const waterLand = solve(waterStartTime, waterDuration, landStartTime, landDuration);
+  return Math.min(landWater, waterLand);
+}
+console.log({ earliestFinishTime: earliestFinishTime([2, 8], [4, 1], [6], [3]) });
