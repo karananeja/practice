74755e29f5fd08e9ca3f9f491b40613447b55a04
karananeja <karananeja.r@gmail.com>
2026-08-15 13:54:07 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -23336,3 +23336,51 @@ function processStr(s) {
   return result.join("");
 }
 console.log({ processStr: processStr("jw%m*h*njc") });
+
+/**
+ * @param {string} s 
+ * @param {number} k 
+ * @returns {string}
+ */
+function processStr(s, k) {
+  let len = 0;
+
+  for (const c of s) {
+    if (c === '*') {
+      if (len > 0) {
+        len--;
+      }
+    } else if (c === '#') {
+      len *= 2;
+    } else if (c === '%') {
+      // no change
+    } else {
+      len++;
+    }
+  }
+
+  if (k + 1 > len) return '.';
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const c = s[i];
+
+    if (c === '*') {
+      len++;
+    } else if (c === '#') {
+      if (k + 1 > (len + 1) / 2) {
+        k -= Math.floor(len / 2);
+      }
+      len = Math.floor((len + 1) / 2);
+    } else if (c === '%') {
+      k = len - k - 1;
+    } else {
+      if (k + 1 === len) {
+        return c;
+      }
+      len--;
+    }
+  }
+
+  return '.';
+}
+console.log({ processStr: processStr('jw%m*h*njc') });
