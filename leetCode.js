0af0df4d5b03383631471944d790672e6ebfe19a
karananeja <karananeja.r@gmail.com>
2026-08-27 10:20:34 +0530

    solved a problem on leetcode

--- leetCode.js
+++ leetCode.js
@@ -23654,3 +23654,35 @@ function findCenter(edges) {
     : firstEdge[1];
 }
 console.log({ findCenter: findCenter([[1, 2], [2, 3], [4, 2]]) });
+
+/**
+ * @param {number[]} nums
+ * @returns {number}
+ */
+function maximumLength(nums) {
+  const cnt = new Map();
+
+  for (const num of nums) {
+    cnt.set(num, (cnt.get(num) ?? 0) + 1);
+  }
+
+  const oneCnt = cnt.get(1) ?? 0;
+  let ans = oneCnt % 2 ? oneCnt : oneCnt - 1;
+
+  cnt.delete(1);
+
+  for (const num of cnt.keys()) {
+    let res = 0;
+    let x = num;
+
+    while (cnt.has(x) && cnt.get(x) > 1) {
+      res += 2;
+      x *= x;
+    }
+
+    ans = Math.max(ans, res + (cnt.has(x) ? 1 : -1));
+  }
+
+  return ans;
+}
+console.log({ maximumLength: maximumLength([5, 4, 1, 2, 2]) });
